1440d5877e59a33b5081dfaaad3cee422c65492b
mukhtharcm <mukhtharcm@gmail.com>
2024-10-27 18:12:58 +0530

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
-import 'dart:io';
-
 import 'package:finapp/themes/modern_purple_theme.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:finapp/screens/auth_wrapper.dart';
-import 'package:finapp/services/auth_service.dart';
-import 'package:finapp/services/finance_service.dart';
-import 'package:pocketbase/pocketbase.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:finapp/service_locator.dart';
 import 'package:finapp/themes/classic_blue_theme.dart';
 
